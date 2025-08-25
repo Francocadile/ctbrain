cd3c0cf7ccd8a66dee754a5e58da2d2379e7e9d9
Francocadile <104653060+Francocadile@users.noreply.github.com>
2025-08-25 14:23:30 -0300

    Create TopRightLogout.tsx

---
+++ src/components/auth/TopRightLogout.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import LogoutButton from "./LogoutButton";
+
+export default function TopRightLogout() {
+  return (
+    <div className="absolute top-4 right-4">
+      <LogoutButton />
+    </div>
+  );
+}
