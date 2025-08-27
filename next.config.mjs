3038789fc5e3f2072c515da3434a077d651aa058
Francocadile <104653060+Francocadile@users.noreply.github.com>
2025-08-26 21:43:03 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,15 @@
+import path from "path";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    typedRoutes: true
-  }
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      "@": path.resolve(process.cwd(), "src"),
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
+
