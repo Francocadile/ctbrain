8eb543c4a4f4bc3ff3a22f410739e7f9197e3847
Francocadile <104653060+Francocadile@users.noreply.github.com>
2025-08-26 22:04:52 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,11 @@
 module.exports = {
   content: [
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}"
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}"
   ],
-  theme: { extend: {} },
-  plugins: []
-}
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
