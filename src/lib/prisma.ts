9a4651030901cd58b874e5dba3c94fd1ea03107c
Francocadile <104653060+Francocadile@users.noreply.github.com>
2025-08-28 09:51:08 -0300

    Update prisma.ts

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,18 +1,11 @@
-// src/lib/prisma.ts
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    // log: ["query", "error", "warn"] // opcional para debug
+    log: ["warn", "error"],
   });
 
-if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma;
-}
-
-// default export (para imports como `import prismaSingleton from "../../../../lib/prisma"`)
-const prismaSingleton = prisma;
-export default prismaSingleton;
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
