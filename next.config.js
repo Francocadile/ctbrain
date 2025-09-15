b4d72987b4ab02f61f985c72bc8a363c0468ad13
Francocadile <104653060+Francocadile@users.noreply.github.com>
2025-09-15 17:19:30 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,16 @@
+// next.config.js
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    // Hace que Vercel no requiera ESLint durante el build
-    ignoreDuringBuilds: true,
-  },
-  // Opcional: si preferís que Next no intente exportar estático páginas con searchParams
-  experimental: {
-    typedRoutes: true,
-  },
+  webpack: (config) => {
+    // Evitar que PDF.js ESM intente importar el worker real en SSR
+    config.resolve.alias["pdfjs-dist/build/pdf.worker.mjs"] = path.resolve(
+      __dirname,
+      "src/lib/pdf.worker.stub.js"
+    );
+    return config;
+  }
 };
 
 module.exports = nextConfig;
