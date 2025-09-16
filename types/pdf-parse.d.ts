d3551bf4e37207d8979d74dbf24453d9283070b0
Francocadile <104653060+Francocadile@users.noreply.github.com>
2025-09-16 11:39:01 -0300

    Update pdf-parse.d.ts

--- types/pdf-parse.d.ts
+++ types/pdf-parse.d.ts
@@ -1,4 +1,4 @@
-declare module 'pdf-parse' {
+declare module "pdf-parse/lib/pdf-parse.js" {
   export interface PDFInfo {
     PDFFormatVersion?: string;
     IsAcroFormPresent?: boolean;
@@ -28,5 +28,6 @@ declare module 'pdf-parse' {
 
   export type PDFSource = Buffer | Uint8Array | ArrayBuffer;
 
-  export default function pdf(dataBuffer: PDFSource, options?: any): Promise<PDFData>;
+  const pdfParse: (dataBuffer: PDFSource, options?: any) => Promise<PDFData>;
+  export default pdfParse;
 }
