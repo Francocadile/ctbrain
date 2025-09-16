8133e4ad1fd6b9f565071b8cde7db03bd46a4f78
Francocadile <104653060+Francocadile@users.noreply.github.com>
2025-09-16 12:04:03 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false
+  reactStrictMode: false,
+
+  // Evitar que falle el build por no tener ESLint instalado/configurado
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
