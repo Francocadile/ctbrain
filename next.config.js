e163a7f84d7689976f2060566a7feb118afa308f
Francocadile <104653060+Francocadile@users.noreply.github.com>
2025-09-16 12:40:21 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
   eslint: { ignoreDuringBuilds: true },
+  reactStrictMode: false,
 };
 
 module.exports = nextConfig;
