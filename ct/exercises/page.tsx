d7bca876697b56f5902dd13b61352c12c36e55ee
Francocadile <franco.cadile@gmail.com>
2025-10-15 19:49:44 -0300

    otra avez yo

--- ct/exercises/page.tsx
+++
@@ -1,3 +0,0 @@
-// Mantiene la URL /ct/exercises pero renderiza la misma UI que /ct/sessions/buscar
-export { default } from "@/app/ct/sessions/buscar/page";
-export const dynamic = "force-dynamic";
