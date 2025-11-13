181806a42fe7848f5eaa65e992379ceefab3108f
Francocadile <franco.cadile@gmail.com>
2025-11-13 17:45:10 -0300

    fix: excluyo archivos de build y cache del repo (.next, node_modules)

--- .next/static/chunks/app/admin/users/pending/page-d7f9db22f3c3cd35.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[3159,8815,1931,8325,3,750,5860,5676,8552,4593,9674],{3521:function(){}},function(n){n.O(0,[2971,7023,1744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
