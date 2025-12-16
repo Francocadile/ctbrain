8a4c139714fd6b0170bcc83c3d97505fe6a5ab41
Francocadile <franco.cadile@gmail.com>
2025-12-16 14:36:37 -0300

    fix(android): register PushNotifications plugin in MainActivity

--- apps/mobile/android/app/src/main/java/com/openbase/mobile/MainActivity.java
+++ apps/mobile/android/app/src/main/java/com/openbase/mobile/MainActivity.java
@@ -1,5 +1,14 @@
 package com.openbase.mobile;
 
-import com.getcapacitor.BridgeActivity;
+import android.os.Bundle;
 
-public class MainActivity extends BridgeActivity {}
+import com.getcapacitor.BridgeActivity;
+import com.capacitorjs.plugins.pushnotifications.PushNotificationsPlugin;
+
+public class MainActivity extends BridgeActivity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		registerPlugin(PushNotificationsPlugin.class);
+		super.onCreate(savedInstanceState);
+	}
+}
